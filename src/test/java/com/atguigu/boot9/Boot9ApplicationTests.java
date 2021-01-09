0445850f43ceaa9e897fd55202fc58a4f47b0b54
JiaZhao-coder <1803843910@qq.com>
2021-01-09 22:55:21 +0800

    贾钊第一次提交基于spring-boot的水果商城项目代码

---
+++ src/test/java/com/atguigu/boot9/Boot9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.boot9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
